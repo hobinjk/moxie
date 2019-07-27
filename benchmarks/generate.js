46259851d78146464277fc1db6f981e01abdd51a
James Hobin <hobinjk@gmail.com>
2019-07-27 00:19:47 -0400

    Add a cache generation script for benchmark skills

---
+++ benchmarks/generate.js
@@ -0,0 +1,50 @@
+let EIParser = require('../EIParser.js');
+
+let benches = [
+  require('./berserker_condi.json'),
+  require('./berserker_condi_banners.json'),
+  require('./berserker_power.json'),
+  require('./berserker_power_banners.json'),
+  require('./chrono_condi.json'),
+  require('./chrono_condi_boon.json'),
+  require('./chrono_power_boon.json'),
+  require('./chrono_power_domi.json'),
+  require('./chrono_power_illu.json'),
+  require('./daredevil_condi.json'),
+  require('./daredevil_power.json'),
+  require('./dragonhunter.json'),
+  require('./firebrand_condi.json'),
+  require('./firebrand_condi_quick.json'),
+  require('./firebrand_power_quick.json'),
+  require('./herald_boon.json'),
+  require('./holo_condi.json'),
+  require('./mirage.json'),
+  require('./reaper.json'),
+  require('./renegade_alac.json'),
+  require('./renegade_kalla.json'),
+  require('./renegade_shiro.json'),
+  require('./soulbeast_power.json'),
+  require('./tempest_power_tempest.json'),
+  require('./weaver_condi_dagger.json'),
+  require('./weaver_condi_sword.json'),
+  require('./weaver_power_btth_large.json'),
+  require('./weaver_power_btth_small.json'),
+  require('./weaver_power_fa_large.json'),
+  require('./weaver_power_fa_small.json'),
+];
+
+let skills = {};
+
+for (const bench of benches) {
+  const log = EIParser.parseJson(bench);
+  console.log(log);
+  for (let id in log.casts) {
+    for (let cast of log.casts[id]) {
+      skills[cast.id] = true;
+    }
+  }
+}
+
+for (let id in skills) {
+  console.log(`curl https://api.guildwars2.com/v2/skills/${id} > ${id}.json`);
+}
