187759d267fa1bcef587f5ad03715332ce0fea4f
James Hobin <hobinjk@gmail.com>
2020-08-05 10:24:58 -0400

    Stop adjusting for LN settings with rifle de

--- benchmark.js
+++ benchmark.js
@@ -9,12 +9,6 @@ async function get(id, name) {
   log.casts = log.casts[0];
   log.buffs = log.buffs[0];
   log.targetDamage1S = log.targetDamage1S[0];
-  if (id === 'deadeye_rifle') {
-    // Adjust DPS for LN settings
-    for (let i = 0; i < log.targetDamage1S; i++) {
-      log.targetDamage1S[i] = log.targetDamage1S[i] * 38100 / 40720;
-    }
-  }
   log.id = id;
   if (!name) {
     log.name = id.replace(/_/g, ' ');
