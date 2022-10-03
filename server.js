d88cb414593985f7ccd237a119b8fe983ca63ef0
James Hobin <hobinjk@gmail.com>
2022-10-03 19:19:44 -0400

    Updates

--- server.js
+++
@@ -1,9 +0,0 @@
-// Simple server with correct content-type from stackoverflow
-
-const express = require('express');
-const app = express();
-
-express.static.mime.define({'application/wasm': ['wasm']});
-
-app.use(express.static('./dist'));
-app.listen(8081);
