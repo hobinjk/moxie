9b7206f134e0a65b48292969996eb3ab6cfb28fa
James Hobin <hobinjk@gmail.com>
2023-02-04 15:54:39 -0500

    Replace generate.sh with js object

---
+++ benchmarks/generate-sh.mjs
@@ -0,0 +1,8 @@
+import {benchmarks} from './benchmarks.mjs';
+
+for (const benchmarkKey in benchmarks) {
+  const benchmark = benchmarks[benchmarkKey];
+  console.log(`# ${benchmark.author} ${benchmark.link}`);
+  let slug = benchmark.log.match(/dps.report\/(.+)$/)[1];
+  console.log(`curl https://dps.report/getJson?permalink=${slug} > ${benchmarkKey}.json`);
+}
