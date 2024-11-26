a55e2ea29f2f0ce5770d6ac10caf821448f5aa89
James Hobin <hobinjk@gmail.com>
2024-11-26 14:00:46 -0500

    Update benchmark scripts

--- benchmarks/generate-sh.js
+++ benchmarks/generate-sh.js
@@ -1,9 +1,9 @@
-import {benchmarks} from './benchmarks.js';
+import { benchmarks } from './benchmarks.js';
 
 for (const benchmarkKey in benchmarks) {
   const benchmark = benchmarks[benchmarkKey];
   console.log(`# ${benchmark.author} ${benchmark.link}`);
   let slug = benchmark.log.match(/dps.report\/(.+)$/)[1];
   console.log(`curl https://dps.report/getJson?permalink=${slug} > ${benchmarkKey}.json`);
-  console.log(`sleep 1`);
+  console.log(`sleep 5`);
 }
