2be1c34094ea50c2590707f3ded9f5625e28a940
James Hobin <hobinjk@gmail.com>
2025-11-09 14:41:34 -0500

    Remove validSpec filtering

--- TargetSelect.js
+++ TargetSelect.js
@@ -1,48 +1,8 @@
-const validSpecs = {
-  // Elementalist: true,
-  Tempest: true,
-  Weaver: true,
-  Catalyst: true,
-  // Mesmer: true,
-  Chronomancer: true,
-  Mirage: true,
-  Virtuoso: true,
-  // Necromancer: true,
-  Reaper: true,
-  Scourge: true,
-  Harbinger: true,
-  // Engineer: true,
-  Scrapper: true,
-  Holosmith: true,
-  Mechanist: true,
-  // Ranger: true,
-  // Druid: true,
-  Soulbeast: true,
-  Untamed: true,
-  // Thief: true,
-  Daredevil: true,
-  Deadeye: true,
-  Specter: true,
-  Guardian: true,
-  Dragonhunter: true,
-  Firebrand: true,
-  Willbender: true,
-  // Revenant: true,
-  Herald: true,
-  Renegade: true,
-  Vindicator: true,
-  // Warrior: true,
-  Berserker: true,
-  Spellbreaker: true,
-  Bladesworn: true,
-};
-
 export default class TargetSelect {
   constructor(log) {
     this.boss = log.boss || 'Unknown';
     console.log(log.players);
-    this.players = log.players
-      .filter(player => validSpecs[player.profession]);
+    this.players = log.players;
     this.onSelect = this.onSelect.bind(this);
     this.elt = null;
     this.playerSelect = null;
